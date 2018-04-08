5d6f9b66366a1ba010c21866d800dad785392895
Sanjiv Kumar <sanjiv@ccs.neu.edu>
2018-04-07 21:21:11 -0400

    Modified structure

--- src/app/widget.component.ts
+++ src/app/widget.component.ts
@@ -3,20 +3,10 @@ import { Component, ViewChild, Input } from '@angular/core';
 @Component({
   selector: 'app-widget',
   template: `
-   <mat-card fxFlex="33%%">
-      <mat-card-header>
-        <mat-card-title>Widget1</mat-card-title>
-      </mat-card-header>
-      <mat-card-content>
-        <p>I am widget {{widgetNumber}} and created dynamically here. I will be draggable in future.</p>
-      </mat-card-content>
-      <mat-card-actions>
-        <button mat-button>Remove</button>
-        <button mat-button>Submit</button>
-      </mat-card-actions>
-    </mat-card>
+  <ng-container [ngTemplateOutlet]="template" [ngTemplateOutletContext]="{data: dataContext}"></ng-container>
   `
 })
 export class WidgetComponent {
-  @Input() widgetNumber: number;
+  @Input() template: number;
+  @Input() dataContext: number;
 }
