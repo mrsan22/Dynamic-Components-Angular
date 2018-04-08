a07a9ce11b996580618cfb2bd9f619a1b5ebed16
Sanjiv Kumar <sanjiv@ccs.neu.edu>
2018-04-07 21:37:40 -0400

    Code for creating an instance of a dynamic component

--- src/app/widget-container.component.ts
+++ src/app/widget-container.component.ts
@@ -1,9 +1,11 @@
-import { Component, ViewChild, Input } from '@angular/core';
+import { Component, ViewChild, Input, ComponentFactoryResolver } from '@angular/core';
+
 import { DynamicWidgetAnchorDirective } from './dynamic-widget-anchor.directive';
+import { WidgetComponent } from './widget.component';
+
 @Component({
   selector: 'app-widget-container',
   template: `
-    <ng-container [ngTemplateOutlet]="template" [ngTemplateOutletContext]="{data: dataContext}"></ng-container>
     <!-- Placeholder to add all the widgets dynamically-->
     <ng-template appDynamicWidgetAnchor></ng-template>
   `
@@ -13,8 +15,25 @@ export class WidgetContainerComponent {
   @Input() dataContext;
 
   @ViewChild(DynamicWidgetAnchorDirective) dynamicWidgetPlaceholder: DynamicWidgetAnchorDirective;
+  // Keep track of all the widget component
+  dynamicWidgetComponents: WidgetComponent[] = [];
 
-  addWidget() {
-    console.log(this.dynamicWidgetPlaceholder.viewContainer);
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
+
+  addWidget(title, template, data) {
+    // create a factory and pass on the component (which you want to create dynamically). In this case it is `WidgetComponent`.
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(WidgetComponent);
+    // This allows to attach componens dynamically - ViewContainerRef
+    const viewContainerRef = this.dynamicWidgetPlaceholder.viewContainer;
+    // Reference to a component i.e. componentRef instance
+    const componentRef = viewContainerRef.createComponent(componentFactory);
+    // To get the instance of our widget component instance
+    const instance: WidgetComponent = componentRef.instance as WidgetComponent;
+    // provide all the required data
+    instance.dataContext = data;
+    instance.template = template;
+
+    // Add the widget component instance here.
+    this.dynamicWidgetComponents.push(instance);
   }
 }
