ab3377e76f626709f8d50a1c4f0e9334166897af
Sanjiv Kumar <sanjiv@ccs.neu.edu>
2018-04-07 21:16:06 -0400

    Adding a new generic widget component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { Widget1Component } from './widget1.component';
 import { Widget2Component } from './widget2.component';
+import { WidgetComponent } from './widget.component';
 import { WidgetContainerComponent } from './widget-container.component';
 import { DynamicWidgetAnchorDirective } from './dynamic-widget-anchor.directive';
 
 @NgModule({
-  declarations: [AppComponent, Widget1Component, Widget2Component, WidgetContainerComponent, DynamicWidgetAnchorDirective],
+  declarations: [
+    AppComponent,
+    Widget1Component,
+    Widget2Component,
+    WidgetComponent,
+    WidgetContainerComponent,
+    DynamicWidgetAnchorDirective
+  ],
   imports: [BrowserModule, MatCardModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent]
